54cc818409038d12f6cccc84ae648632797b50ae
astadipati <asekbos@gmail.com>
2022-09-03 17:06:12 +0700

    getAll

--- routes/handler/media/getAll.js
+++ routes/handler/media/getAll.js
@@ -8,7 +8,7 @@ const api = apiAdapter(URL_SERVICE_MEDIA);
 
 module.exports = async (req, res) => {
     try {
-        const media = await api.post('/media', req.body);
+        const media = await api.get('/media');
         return res.json(media.data);
     } catch (error) {
 
--- routes/media.js
+++ routes/media.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const mediaHandler = require('./handler/media');
 
 router.post('/', mediaHandler.create);
+router.get('/', mediaHandler.getAll);
 
 module.exports = router;
